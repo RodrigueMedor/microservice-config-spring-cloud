5a3cda732c3e515674db3e8573914b635f64ced7
rmedor <rmedor@nexient.com>
2021-10-10 21:03:14 -0400

    final request

--- cartsService/src/main/java/com/cart/cartsService/CartsServiceApplication.java
+++ cartsService/src/main/java/com/cart/cartsService/CartsServiceApplication.java
@@ -2,8 +2,15 @@ package com.cart.cartsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScans({@ComponentScan("com.cart.cartsService.controller")})
+@EnableJpaRepositories("com.cart.cartsService.repository")
+@EntityScan("com.cart.cartsService.domain")
 public class CartsServiceApplication {
 
 	public static void main(String[] args) {
